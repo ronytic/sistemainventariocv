e15bfb556cb65905e7a4670124e38a31c2d4f13e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-06-14 13:37:29 -0400

    Pie

---
+++ pie.php
@@ -0,0 +1,49 @@
+             </div>
+            </div>
+
+          </div>
+          <!-- / Content -->
+
+          <!-- Layout footer -->
+          <nav class="layout-footer footer bg-footer-theme">
+            <div class="container-fluid d-flex flex-wrap justify-content-between text-center container-p-x pb-3">
+              <div class="pt-3">
+                <span class="footer-text font-weight-bolder"><?=$TituloSistema;?> ©</span> 
+              </div>
+              <div>
+                Sistema Desarrollado por 
+                <a href="http://fb.com/ronaldnina/" target="_blank" class="footer-link pt-3 ml-4">Ronald Nina</a>
+              </div>
+            </div>
+          </nav>
+          <!-- / Layout footer -->
+
+        </div>
+        <!-- Layout content -->
+
+      </div>
+      <!-- / Layout container -->
+
+    </div>r
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-sidenav-toggle"></div>
+  </div>
+  <!-- / Layout wrapper -->
+
+  <!-- Core scripts -->
+  <script src="<?=$folder;?>js/core/popper/popper.js"></script>
+  <script src="<?=$folder;?>js/core/bootstrap.js"></script>
+  <script src="<?=$folder;?>js/core/sidenav.js"></script>
+
+  <!-- Libs -->
+  <script src="<?=$folder;?>js/core/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+  <!-- Demo -->
+  <script src="<?=$folder;?>js/core/demo.js"></script>
+
+</body>
+
+
+<!-- Mirrored from uxpowered.com/products/appwork/v100/html-demo/forms_layouts.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 07 Jun 2018 14:54:22 GMT -->
+</html>
