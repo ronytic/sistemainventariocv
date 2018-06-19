effb8ee4e53fbad4d920b6d470fd3a80f0648e49
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-06-19 15:42:05 -0400

    Modificar Compra

---
+++ compras/registro/actualizar.php
@@ -0,0 +1,28 @@
+<?php
+include_once("../../login/check.php");
+extract($_POST);
+include_once("../../class/articulo.php");
+$articulo=new articulo;
+
+$valores=array("codgrupo"=>"'$codgrupo'",
+                "codigointerno"=>"'$codigointerno'",
+               "nombre"=>"'$nombre'",
+               "codunidad"=>"'$codunidad'",
+               "preciominimo"=>"'$preciominimo'",
+               "precioventa"=>"'$precioventa'",
+               "stockminimo"=>"'$stockminimo'",
+              "stockmaximo"=>"'$stockmaximo'",
+               "detalle"=>"'$detalle'",
+            );
+
+
+$articulo->actualizarRegistro($valores,"codarticulo=".$cod);
+//print_r($valores);
+$titulo="Mensaje de Confirmación";
+$folder="../../";
+$nuevo=1;
+$listar=1;
+//$botones=array("Facturar"=>array("facturar.php","danger"));
+$mensajes[]="Sus datos fueron guardados correctamente.";
+include_once("../../respuesta.php");
+?>
