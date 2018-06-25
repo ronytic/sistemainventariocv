51c738767d1625d7dcaf1cef0cbce186706763c3
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-06-25 00:39:08 -0400

    Compra Detalle

--- class/compradetalle.php
+++ class/compradetalle.php
@@ -3,6 +3,9 @@ include_once("bd.php");
 class compradetalle extends bd{
 	var $tabla="compradetalle";
 
-	
+	function cantidadCompras($codarticulo){
+        $this->campos=array('sum(cantidad) as CompraTotal');
+        return $this->mostrarTodoRegistro("codarticulo=$codarticulo",1,"");
+    }
 }
 ?>
