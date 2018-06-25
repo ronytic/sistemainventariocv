7c7d344066700873631cd08abe22e1b7d573a6db
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-06-25 00:39:48 -0400

    Boleta

---
+++ venta/registro/boleta.php
@@ -0,0 +1,14 @@
+<?php
+include_once("../../login/check.php");
+$folder="../../";
+$codventa=$_GET['codventa'];
+$url="reporte.php?codventa=".$codventa;
+
+$titulo="Reporte de Venta";
+?>
+<?php require_once($folder."cabecerahtml.php")?>
+
+<?php require_once($folder."cabecera.php")?>
+<a href="<?=$url?>" target="_blank" class="btn btn-success btn-xs">Abrir en una nueva Ventana</a>
+<iframe src="<?=$url?>" frameborder="0" width="100%" height="600"></iframe>
+<?php require_once($folder."pie.php")?>
