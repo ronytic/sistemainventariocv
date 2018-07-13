10771918f65abb2a888b7c47e7e943452933dc48
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-07-13 10:40:47 -0400

    Cargador Total

--- js/core/cargadortotal.js
+++ js/core/cargadortotal.js
@@ -40,8 +40,13 @@ function inicio(){
 		e.preventDefault();
 		e.stopPropagation();
 		if(confirm("¿Esta seguro que desea Eliminar este Registro?")){
-			$.post(direccion,function(){
-				$("form.formulariobusqueda").submit();	
+			$.post(direccion,function(data){
+                if(data==""){
+                    
+				    $("form.formulariobusqueda").submit();
+                }else{
+                    alert(data);
+                }
 			});
 		}
 		return false;
@@ -54,4 +59,56 @@ function inicio(){
 			return false;
 		}
 	});
+
+    
+    /*Exportar Excel*/
+    
+	$(document).on('click','.exportarexcel',function(e){
+
+		e.preventDefault();
+		var tabla=$(this).next().clone();
+		if(!(tabla.find("thead:eq(0)")).length){
+			tabla=$(this).next().next().clone();
+		}
+		
+		//alert(tabla.find("thead").html().remove());
+		var html='<table border="1">'+tabla.html()+'</table>';
+        //alert(html);
+        html.replace('Ã³', 'ó');
+        //alert(html);
+		//return false;
+		while (html.indexOf('Ã¡') != -1) html = html.replace('Ã¡', 'á');
+		while (html.indexOf('Ã©') != -1) html = html.replace('Ã©', 'é');
+		while (html.indexOf('Ã­') != -1) html = html.replace('Ã­', 'í');
+		while (html.indexOf('Ã³') != -1) html = html.replace('Ã³', 'ó');
+		while (html.indexOf('Ãº') != -1) html = html.replace('Ãº', 'ú');
+		while (html.indexOf('Ã±') != -1) html = html.replace('Ã±', 'ñ');
+		while (html.indexOf('Âº') != -1) html = html.replace('Âº', 'º');
+		/*window.open('data:application/vnd.ms-excel;Content-Disposition:attachment;file=export_filename.xls;name=hebe.xls,' +encodeURIComponent(html));
+    e.preventDefault();
+		//$.post(folder+"exportar/excel.php",{'dataexcel':datos},function(data){$("#respuestaexcel").html(data)});		*/
+		//getting values of current time for generating the file name
+        var dt = new Date();
+        var day = dt.getDate();
+        var month = dt.getMonth() + 1;
+        var year = dt.getFullYear();
+        var hour = dt.getHours();
+        var mins = dt.getMinutes();
+        var postfix = day + "." + month + "." + year + "_" + hour + "." + mins;
+        //creating a temporary HTML link element (they support setting file names)
+        var a = document.createElement('a');
+        //getting data from our div that contains the HTML table
+        var data_type = 'data:application/vnd.ms-excel';
+       // var table_div = $(this).next("table");
+        var table_html = html.replace(/ /g, '%20');
+        a.href = data_type + ', ' + table_html;
+        //setting the file name
+        a.download = 'Reporte'+'_' + postfix + '.xls';
+        //triggering the function
+        a.click();
+        //just in case, prevent default behaviour
+        e.preventDefault();
+	});
+	/*Fin de Exportar Excel*/
+    $('select').select2();
 }
