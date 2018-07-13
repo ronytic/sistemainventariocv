782ab610ec4898f2aaa1529a0041c509feea986b
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-07-13 10:39:08 -0400

    Prvoeedor Eliminar

--- proveedores/registro/eliminar.php
+++ proveedores/registro/eliminar.php
@@ -3,5 +3,13 @@ include_once("../../login/check.php");
 include_once("../../class/proveedor.php");
 $Cod=$_GET['Cod'];
 $proveedor=new proveedor;
-$proveedor->eliminarRegistro("codproveedor=".$Cod);
+include_once("../../class/compra.php");
+$compra=new compra;
+$c=$compra->mostrarTodoRegistro("codproveedor=".$Cod);
+if(count($c)==0){
+    $proveedor->eliminarRegistro("codproveedor=".$Cod);
+}else{
+    echo "Este Proveedor TIENE REGISTRADO compras, no se puede Eliminar";
+}
+
 ?>
