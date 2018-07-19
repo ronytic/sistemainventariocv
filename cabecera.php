e84e9602820489afe4bb4f8be05fa4d3b4cdccaf
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-07-19 15:23:29 -0400

    Cabecera

--- cabecera.php
+++ cabecera.php
@@ -71,7 +71,7 @@
             <span class="demo-brand-logo bg-primary">
               <svg viewBox="0 0 148 80" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><linearGradient id="a" x1="46.49" x2="62.46" y1="53.39" y2="48.2" gradientUnits="userSpaceOnUse"><stop stop-opacity=".25" offset="0"></stop><stop stop-opacity=".1" offset=".3"></stop><stop stop-opacity="0" offset=".9"></stop></linearGradient><linearGradient id="e" x1="76.9" x2="92.64" y1="26.38" y2="31.49" xlink:href="#a"></linearGradient><linearGradient id="d" x1="107.12" x2="122.74" y1="53.41" y2="48.33" xlink:href="#a"></linearGradient></defs><path style="fill: #fff;" transform="translate(-.1)" d="M121.36,0,104.42,45.08,88.71,3.28A5.09,5.09,0,0,0,83.93,0H64.27A5.09,5.09,0,0,0,59.5,3.28L43.79,45.08,26.85,0H.1L29.43,76.74A5.09,5.09,0,0,0,34.19,80H53.39a5.09,5.09,0,0,0,4.77-3.26L74.1,35l16,41.74A5.09,5.09,0,0,0,94.82,80h18.95a5.09,5.09,0,0,0,4.76-3.24L148.1,0Z"></path><path transform="translate(-.1)" d="M52.19,22.73l-8.4,22.35L56.51,78.94a5,5,0,0,0,1.64-2.19l7.34-19.2Z" fill="url(#a)"></path><path transform="translate(-.1)" d="M95.73,22l-7-18.69a5,5,0,0,0-1.64-2.21L74.1,35l8.33,21.79Z" fill="url(#e)"></path><path transform="translate(-.1)" d="M112.73,23l-8.31,22.12,12.66,33.7a5,5,0,0,0,1.45-2l7.3-18.93Z" fill="url(#d)"></path></svg>
             </span>
-            <span class="demo-brand-name font-weight-normal ml-2">Appwork</span>
+            <span class="demo-brand-name font-weight-normal ml-2">SINCOVE</span>
           </a>
 
           <!-- Sidenav toggle (see assets/css/demo/demo.css) -->
@@ -90,16 +90,17 @@
             <hr class="d-lg-none w-100 my-2">
 
             <div class="navbar-nav align-items-lg-center">
-              <!-- Search -->
+              <!-- Search --><!--
               <label class="nav-item navbar-text navbar-search-box p-0 active">
                 <i class="ion ion-ios-search navbar-icon align-middle"></i>
                 <span class="navbar-search-input pl-2">
                   <input type="text" class="form-control navbar-text mx-2" placeholder="Buscar..." style="width:200px">
                 </span>
-              </label>
+              </label>-->
             </div>
 
             <div class="navbar-nav align-items-lg-center ml-auto">
+             <!--
               <div class="demo-navbar-notifications nav-item dropdown mr-lg-3">
                 <a class="nav-link dropdown-toggle hide-arrow" href="#" data-toggle="dropdown">
                   <i class="ion ion-md-notifications-outline navbar-icon align-middle"></i>
@@ -160,8 +161,8 @@
                   <a href="javascript:void(0)" class="d-block text-center text-light small p-2 my-1">Show all notifications</a>
                 </div>
               </div>
-
-              <div class="demo-navbar-messages nav-item dropdown mr-lg-3">
+            -->
+              <!--<div class="demo-navbar-messages nav-item dropdown mr-lg-3">
                 <a class="nav-link dropdown-toggle hide-arrow" href="#" data-toggle="dropdown">
                   <i class="ion ion-ios-mail navbar-icon align-middle"></i>
                   <span class="badge badge-primary badge-dot indicator"></span>
@@ -215,7 +216,7 @@
 
                   <a href="javascript:void(0)" class="d-block text-center text-light small p-2 my-1">Show all messages</a>
                 </div>
-              </div>
+              </div>-->
 
               <!-- Divider -->
               <div class="nav-item d-none d-lg-block text-big font-weight-light line-height-1 opacity-25 mr-3 ml-1">|</div>
@@ -228,13 +229,13 @@
                   </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
-                  <a href="javascript:void(0)" class="dropdown-item">
+                  <!--<a href="javascript:void(0)" class="dropdown-item">
                     <i class="ion ion-ios-person text-lightest"></i> &nbsp; Mi Perfil</a>
                   <a href="javascript:void(0)" class="dropdown-item">
                     <i class="ion ion-ios-mail text-lightest"></i> &nbsp; Mensajes</a>
                   <a href="javascript:void(0)" class="dropdown-item">
                     <i class="ion ion-md-settings text-lightest"></i> &nbsp; Configuración</a>
-                  <div class="dropdown-divider"></div>
+                  <div class="dropdown-divider"></div>-->
                   <a href="<?=$folder?>login/logout.php" class="dropdown-item">
                     <i class="ion ion-ios-log-out text-danger"></i> &nbsp; Salir del Sistema</a>
                 </div>
