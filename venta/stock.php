4d059a8345418f3e49bed4a4132602024478b26d
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-07-27 12:45:03 -0400

    Verificar Stock

--- venta/stock.php
+++ venta/stock.php
@@ -1,35 +1,49 @@
 <?php
-function ObtenerStock($codarticulo){
 include_once("../../class/compradetalle.php");
-$compradetalle=new compradetalle;
-$cd=$compradetalle->cantidadCompras($codarticulo);
-$cd=array_shift($cd);
-
-$CompraTotal=$cd['CompraTotal'];
-
-
+    $compradetalle=new compradetalle;
 
 include_once("../../class/ventadetalle.php");
-$ventadetalle=new ventadetalle;
-$vd=$ventadetalle->cantidadVentas($codarticulo);
-$vd=array_shift($vd);
+    $ventadetalle=new ventadetalle;
 
-$CompraTotal=$cd['CompraTotal'];
-$VentaTotal=$vd['VentaTotal'];
-
-    
-    
 include_once("../../class/traspaso.php");
-$traspaso=new traspaso;
-$ts=$traspaso->cantidadSalida($codarticulo);
-$ts=array_shift($ts);   
-$te=$traspaso->cantidadEntrada($codarticulo);
-$te=array_shift($te);   
     
-$TrasSalida=$ts['CantidadSalida'];
+    $traspaso=new traspaso;
+
+function ObtenerStock($codarticulo){
+    global $compradetalle,$ventadetalle,$traspaso;
+    $sstock=1;
+    
+    
+    
+    $cd=$compradetalle->cantidadCompras($codarticulo);
+    $cd=array_shift($cd);
+
+    $CompraTotal=$cd['CompraTotal'];
+
+
+
+    
+    
+    $vd=$ventadetalle->cantidadVentas($codarticulo);
+    $vd=array_shift($vd);
+
+    $CompraTotal=$cd['CompraTotal'];
+    $VentaTotal=$vd['VentaTotal'];
+
+
+
+    
+    
+    $ts=$traspaso->cantidadSalida($codarticulo);
+    $ts=array_shift($ts);   
+    $te=$traspaso->cantidadEntrada($codarticulo);
+    $te=array_shift($te);   
+
+    $TrasSalida=$ts['CantidadSalida'];
     $TrasEntrada=$te['CantidadEntrada'];
     
-    $stock=$CompraTotal-$VentaTotal-$TrasSalida+$TrasEntrada;
-    return $stock;
+    $sstock=$CompraTotal-$VentaTotal-$TrasSalida+$TrasEntrada;
+    
+    return $sstock;
 }
 ?>
